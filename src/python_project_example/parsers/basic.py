bc3cdf3f9df13d8e9f37c03e512bd244a8d026e2
Mystinen Metsätyömies <kataja08@yahoo.com>
2024-01-04 22:11:59 +0200

    Added docs

--- src/python_project_example/parsers/basic.py
+++ src/python_project_example/parsers/basic.py
@@ -1,10 +1,15 @@
 
-from ..util.helper import StringHelper
+"""
+String parsing functions.
+"""
 from typing import Union
+from ..util.helper import StringHelper
 
 
 def parse_str(data: str, lower_case=False) -> Union[str, None]:
-
+    """
+    Split string to a list, use whitespace.
+    """
     if lower_case:
         data = StringHelper.to_lower(data)
         
--- src/python_project_example/util/helper.py
+++ src/python_project_example/util/helper.py
@@ -1,13 +1,22 @@
-
+"""
+Helper tools for string manipulation.
+"""
 
 class StringHelper:
-    
+    """
+    A class for inspecting and checking strings.
+    """
+
     @staticmethod
     def has_spaces(data: str) -> bool:
+        """Check if the string contains spaces."""
         if " " in data:
             return True
-        else:
-            return False
-    
+
+        return False
+
+    @staticmethod
     def to_lower(data: str) -> str:
+        """Convert all characters in a string to lowercase."""
+        
         return data.lower()
